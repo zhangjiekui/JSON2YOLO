1f587dd9619c09e1f22afdcc35276cfec194be24
zhangjiekui <33198334+zhangjiekui@users.noreply.github.com>
2021-11-04 21:32:23 +0800

    np.unique删除重复行，并保证原来顺序

--- unique_labels_using_numpy.py
+++ unique_labels_using_numpy.py
@@ -3,7 +3,10 @@ orgin_file =  r'.\000000201706_unique_test.txt'
 writed_file =  r'.\000000201706_unique_test_rewrite.txt'
 s = np.loadtxt(orgin_file)
 print(f"    {s.shape=}")
-s_unique = np.unique(s ,axis=0)
+_, idx = np.unique(s,axis=0, return_index=True)
+s_unique = s[np.sort(idx)]
+print(s_unique)
+
 print(f"    {s_unique.shape=}")
 np.savetxt(writed_file, s_unique,fmt="%f", delimiter=" ")
 s_unique_loaded = np.loadtxt(writed_file)
